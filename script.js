4ff4a720d114c30f5964c957a051b34a391462b3
DstryThs <134446286+DstryThs@users.noreply.github.com>
2023-08-17 18:01:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const datetimeElement = document.getElementById('datetime');
+
+function updateDateTime() {
+  const currentDateTime = new Date();
+  const formattedDateTime = currentDateTime.toLocaleString('en-US', {
+    weekday: 'long',
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  });
+
+  datetimeElement.textContent = `Current Date and Time: ${formattedDateTime}`;
+  requestAnimationFrame(updateDateTime);
+}
+
+updateDateTime();
 
